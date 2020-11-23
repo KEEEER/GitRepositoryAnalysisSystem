feb2db965fc84b6188ec44e7357263f6501f6643
unknown <t108598076@ntut.org.tw>
2020-11-23 17:12:18 +0800

    [feature] add AngularFilter to conbime Angular and java servlet

---
+++ src/main/java/AngularFilter.java
@@ -0,0 +1,71 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Objects;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/frontEnd/*")
+public class AngularFilter implements Filter {
+    FilterConfig filterConfig;
+
+    public AngularFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void destroy() {
+        this.filterConfig = null;
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String destination,angularRootPath,realPath;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse=(HttpServletResponse)response;
+        ServletContext context = httpRequest.getServletContext();
+        destination =httpRequest.getServletPath();
+
+        realPath = Objects.requireNonNull(this.getClass().getClassLoader().getResource("../../frontEnd/index.html")).getPath();
+        realPath =  URLDecoder.decode(realPath,"utf-8");
+        angularRootPath="";
+
+        System.out.println(destination);
+
+        System.out.println(realPath);
+
+        File f = new File(realPath);
+        if (f.exists()) {
+            chain.doFilter(request, response);
+        } else {
+            FilterRegistration fr= context.getFilterRegistration(this.getClass().getName());
+            for (String mapping: fr.getUrlPatternMappings()) {
+                mapping=mapping.replace("/*", "");
+                if (destination.contains(mapping)) {
+                    angularRootPath=mapping;
+                    break;
+                }
+            }
+            if (angularRootPath.equals("")) {
+                httpResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                filterConfig.getServletContext().getRequestDispatcher(angularRootPath+"/index.html").forward(request, response);
+            }
+        }
+        f=null;
+        System.out.println("eo---------------------------------");
+
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+        this.filterConfig=fConfig;
+    }
+
+}
