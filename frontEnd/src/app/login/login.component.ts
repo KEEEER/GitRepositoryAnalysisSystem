ede16044b5f526b21a03f8037372bd480b608f2e
TedLin <t109598073@ntut.org.tw>
2020-12-22 20:53:08 +0800

    [feature] login page ok

--- frontEnd/src/app/login/login.component.ts
+++ frontEnd/src/app/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './login.html',
+  styleUrls: ['./loginstyle.css']
 })
 export class LoginComponent implements OnInit {
   name = "Hello";
+  backgroudimgURL = 'https://images.pexels.com/photos/669617/pexels-photo-669617.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
   constructor() { }
 
   ngOnInit(): void {
