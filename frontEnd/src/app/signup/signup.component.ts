dcd25ee1f369464a97ef253e12ba6904addf14db
unknown <t108598076@ntut.org.tw>
2020-12-24 15:07:42 +0800

    [fix]SignupPage setting

--- frontEnd/src/app/signup/signup.component.ts
+++ frontEnd/src/app/signup/signup.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
   backgroudimgURL = 'https://images.pexels.com/photos/669617/pexels-photo-669617.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+  UserInput = '';
+  accountInput=';'
+  passwordInput='';
+  RetrypasswordInput = '';
   constructor() { }
 
   ngOnInit(): void {
