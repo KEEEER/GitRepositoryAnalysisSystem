adb2234f264276136a755818685ec9782c9fb05f
TedLin <t109598073@ntut.org.tw>
2020-12-30 15:32:13 +0800

    [delete]webapp/FrondEnd from build

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -10,7 +10,7 @@ public class Database {
     public static Connection getConnection() {
         try {
             new Driver();
-            return DriverManager.getConnection("jdbc:mysql:///gras?user=root&password=password1234&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC");
+            return DriverManager.getConnection("jdbc:mysql://140.124.181.3:3306/gras?user=user3&password=teammate1221&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC");
         } catch (SQLException e) {
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
